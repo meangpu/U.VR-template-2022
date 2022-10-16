8e4ec1679fd6688199fa2f1d93ed06e418c3576b
meangpu <51825872+meangpu@users.noreply.github.com>
2022-10-16 15:47:51 +0700

    update hand animation scpt and pref

--- vr_template_2022/Assets/__Scripts/VRbase/AnimateHandOnInput.cs
+++ vr_template_2022/Assets/__Scripts/VRbase/AnimateHandOnInput.cs
@@ -5,6 +5,19 @@ using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
+    [SerializeField] Animator handAnimator;
     [SerializeField] InputActionProperty pinchAnimationAction;
+    [SerializeField] InputActionProperty gripAnimationAction;
+
+    private void Update()
+    {
+        // ปุ่มตรงนิ้วชี้ ที่อยู่หน้าสุด ทำนิ้ว OK 
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        // ตอนทำท่ากำมือ 
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
 
 }
