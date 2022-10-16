7477692f5529e52a025113f4b2263971c2b59f61
meangpu <51825872+meangpu@users.noreply.github.com>
2022-10-16 22:49:10 +0700

    finish make menu ui rotate follow player

--- vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
+++ vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
@@ -5,15 +5,25 @@ using UnityEngine.InputSystem;
 
 public class GameMenuToggle : MonoBehaviour
 {
+    [SerializeField] Transform head;
+    [SerializeField] float spawnDistance = 2;
+
     [SerializeField] GameObject menu;
     [SerializeField] InputActionProperty showButton;
+
 
     void Update()
     {
         if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive(!menu.activeSelf);
+            Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+
+            menu.transform.position = headLocation;
 
         }
+
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.forward *= -1;
     }
 }
