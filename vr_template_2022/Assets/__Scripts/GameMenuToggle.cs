ac7bdf86dd67c5e51ba8e91720f32cdacf162460
meangpu <51825872+meangpu@users.noreply.github.com>
2022-10-17 23:29:07 +0700

    try fix menu

--- vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
+++ vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
@@ -17,13 +17,12 @@ public class GameMenuToggle : MonoBehaviour
         if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive(!menu.activeSelf);
-            // Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-
-            // menu.transform.position = headLocation;
+            Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+            menu.transform.position = headLocation;
 
         }
 
-        // menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        // menu.transform.forward *= -1;
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.forward *= -1;
     }
 }
