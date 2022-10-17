0bbcad8c6fdc40ea118d9e6f487feaa1987a2517
meangpu <51825872+meangpu@users.noreply.github.com>
2022-10-17 23:35:35 +0700

    fix ui menu canvas

--- vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
+++ vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
@@ -5,8 +5,8 @@ using UnityEngine.InputSystem;
 
 public class GameMenuToggle : MonoBehaviour
 {
-    [SerializeField] Transform head;
-    [SerializeField] float spawnDistance = 2;
+    // [SerializeField] Transform head;
+    // [SerializeField] float spawnDistance = 2;
 
     [SerializeField] GameObject menu;
     public InputActionProperty showButton;
@@ -17,12 +17,12 @@ public class GameMenuToggle : MonoBehaviour
         if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive(!menu.activeSelf);
-            Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-            menu.transform.position = headLocation;
+            // Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+            // menu.transform.position = headLocation;
 
         }
 
-        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        menu.transform.forward *= -1;
+        // menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        // menu.transform.forward *= -1;
     }
 }
