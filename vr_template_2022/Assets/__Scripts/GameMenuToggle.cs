dd9d664ab08905e29d234ac9cfce889cfb3b4cd3
meangpu <51825872+meangpu@users.noreply.github.com>
2022-10-17 23:35:52 +0700

    delete not needed code

--- vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
+++ vr_template_2022/Assets/__Scripts/GameMenuToggle.cs
@@ -5,24 +5,15 @@ using UnityEngine.InputSystem;
 
 public class GameMenuToggle : MonoBehaviour
 {
-    // [SerializeField] Transform head;
-    // [SerializeField] float spawnDistance = 2;
-
     [SerializeField] GameObject menu;
     public InputActionProperty showButton;
-
 
     void Update()
     {
         if (showButton.action.WasPressedThisFrame())
         {
             menu.SetActive(!menu.activeSelf);
-            // Vector3 headLocation = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-            // menu.transform.position = headLocation;
-
         }
 
-        // menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        // menu.transform.forward *= -1;
     }
 }
